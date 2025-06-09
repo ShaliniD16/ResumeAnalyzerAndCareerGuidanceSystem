f70b93fcc2f1608ec766505f7f848b8b22167d04
SHALINI D <119507853+ShaliniD16@users.noreply.github.com>
2025-06-09 23:03:05 +0530

    Update User.java

--- ResumeAnalyzer/src/main/java/com/example/ResumeAnalyzer/models/User.java
+++ ResumeAnalyzer/src/main/java/com/example/ResumeAnalyzer/models/User.java
@@ -14,7 +14,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // Your existing fields
+   
     private String username;
     private String email;
     private String password;
